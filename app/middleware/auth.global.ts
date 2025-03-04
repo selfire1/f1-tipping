44d34fdb864a9d8a7f82a5de4cd0e3570e8b4f56
Joschua <70809675+selfire1@users.noreply.github.com>
2025-03-04 15:48:59 +1000

    make middleware global

---
+++ app/middleware/auth.global.ts
@@ -0,0 +1,17 @@
+export default defineNuxtRouteMiddleware(async (to) => {
+  const isUserNavigatingToTheApp = to.path.startsWith("/tipping");
+  if (!isUserNavigatingToTheApp) {
+    return;
+  }
+
+  const { authClient } = useAuth();
+  const { data: loggedIn } = await authClient.useSession(useFetch);
+  if (!loggedIn.value) {
+    return navigateTo({
+      path: "/auth",
+      query: {
+        ...$getQueryOrigin(QueryOrigin.NotAllowed),
+      },
+    });
+  }
+});
