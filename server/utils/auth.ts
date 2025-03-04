5ae2673903e8e217e028bbbb2cc6ee69cfe19a42
Joschua <70809675+selfire1@users.noreply.github.com>
2025-03-04 15:44:28 +1000

    remove commented code

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -1,12 +1,6 @@
 import { betterAuth } from "better-auth";
-// import { LibsqlDialect } from "@libsql/kysely-libsql";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./db";
-
-// const dialect = new LibsqlDialect({
-//   url: process.env.NUXT_TURSO_DATABASE_URL || "",
-//   authToken: process.env.NUXT_TURSO_AUTH_TOKEN || "",
-// });
 
 export const auth = betterAuth({
   socialProviders: {
@@ -18,8 +12,4 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "sqlite",
   }),
-  // database: {
-  //   dialect,
-  //   type: "sqlite",
-  // },
 });
