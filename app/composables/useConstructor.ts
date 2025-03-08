6d98dfe6aa688e6caa3efe1c2ece328d42632e86
Joschua <70809675+selfire1@users.noreply.github.com>
2025-03-08 17:05:24 +1000

    return nested items

--- app/composables/useConstructor.ts
+++ app/composables/useConstructor.ts
@@ -2,6 +2,7 @@ export const useConstructor = async () => {
   const nuxtApp = useNuxtApp();
 
   const { data: apiConstructors } = await useFetch("/api/constructors", {
+    transform: (data) => data.items,
     getCachedData: (key) => {
       const inCache = nuxtApp.payload.data[key] || nuxtApp.static.data[key];
       console.log("constructors are in cache", !!inCache);
--- app/composables/useDriver.ts
+++ app/composables/useDriver.ts
@@ -2,6 +2,7 @@ export const useDriver = async () => {
   const nuxtApp = useNuxtApp();
 
   const { data: apiDrivers } = await useFetch("/api/drivers", {
+    transform: (data) => data.items,
     getCachedData: (key) => {
       const inCache = nuxtApp.payload.data[key] || nuxtApp.static.data[key];
       console.log("drivers are in cache", !!inCache);
