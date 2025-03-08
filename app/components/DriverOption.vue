c0f001abd65800288c817e35e0f348baad432cd7
Joschua <70809675+selfire1@users.noreply.github.com>
2025-03-08 17:06:02 +1000

    update keys in option

--- app/components/DriverOption.vue
+++ app/components/DriverOption.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-import type { Driver } from "~~/types";
+import type { Database } from "~~/types/db";
+
 defineProps<{
-  option: Driver;
+  option: Database.Driver;
 }>();
 </script>
 
 <template lang="pug">
-.flex.items-center.gap-2.border-l-4.pl-2(:style="{ 'border-color': option.constructor.id ? $getConstructorCssVariable(option.constructor.id) : '' }")
+.flex.items-center.gap-2.border-l-4.pl-2(:style="{ 'border-color': option.constructorId ? $getConstructorCssVariable(option.constructorId) : '' }")
   p
     span.text-muted {{ option.givenName + " " }}
     span.is-size-7 {{ option.familyName }}
