8644ef1eb8376db3b0d5d6c9b8600d16241b7ac8
Joschua <70809675+selfire1@users.noreply.github.com>
2025-03-10 06:50:09 +1000

    show next race on dashboard

--- app/pages/tipping/index.vue
+++ app/pages/tipping/index.vue
@@ -1,15 +1,30 @@
 <script setup lang="ts">
+import BadgeTimeTo from "~/components/BadgeTimeTo.vue";
+import { $getCutoffDate } from "~~/shared/utils";
+
 const { user } = await useAuthUser();
 if (!user?.id) {
   await navigateTo("/auth");
 }
 
-const { allUserGroups } = useGroup();
-
 definePageMeta({
   layout: false,
 });
-const { allRaces } = await useRace();
+
+const { allUserGroups, currentUserGroup } = await useGroup();
+const { getRacesInTheFuture } = await useRace();
+const nextRace = computed(
+  () => getRacesInTheFuture(currentUserGroup.value?.cutoffInMinutes)?.[0],
+);
+
+const cutOffDate = computed(() =>
+  nextRace?.value?.qualifyingDate
+    ? $getCutoffDate(
+        nextRace.value?.qualifyingDate,
+        currentUserGroup.value?.cutoffInMinutes,
+      )
+    : null,
+);
 </script>
 
 <template lang="pug">
@@ -17,11 +32,35 @@ NuxtLayout(name="tipping")
   template(#page-title)
     | Dashboard
   .is-page-height.is-container
-    pre {{ allRaces }}
     .grid.is-grid-card-fit.gap-8
       template(v-if="!allUserGroups?.length")
         UCard
           template(#header)
             TextHero(:level="2" heading="Join or start a group" description="You are not yet a member of a group. Get started tipping by joining or creating a group.")
           UButton(to="/tipping/groups" label="Manage groups")
+      template(v-else)
+        template(v-if="nextRace")
+          UCard
+            template(#header)
+              h2.is-display-7 Next Race
+            template(#footer)
+              UButton(label="Tip now" to="/tipping/add-tips" trailing icon="carbon:arrow-right")
+            .space-y-4
+              .relative.overflow-hidden.py-4.-mx-6.-mt-6
+                NuxtImg.opacity-5.h-full.w-full.object-cover.inset-0.absolute(:src="COUNTRY_FLAGS[nextRace.country]")
+                .px-6
+                  .flex.items-center.justify-between.is-size-8.uppercase.text-muted
+                    p {{ "Round " + nextRace.round }}
+                    p
+                      span {{ nextRace.locality + ", " }}
+                      span {{ nextRace.country }}
+                  p.is-display-5 {{ nextRace.raceName }}
+              .is-size-7(v-if="cutOffDate")
+                p.is-display-7.flex.items-center.gap-2
+                  | Tipping closes
+                  BadgeTimeTo(:date="cutOffDate")
+                .flex
+                  .pl-2
+                    p {{ cutOffDate.toLocaleString(undefined, { hour: "numeric", minute: "2-digit"}) }}
+                    p {{ cutOffDate.toLocaleString(undefined, { year: "numeric", month: "short", day: "numeric" }) }}
 </template>
