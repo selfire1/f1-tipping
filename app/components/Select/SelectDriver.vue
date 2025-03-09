364aa3b68e529de879a101ba4f63c7aec0ea09ef
Joschua <70809675+selfire1@users.noreply.github.com>
2025-03-10 07:53:18 +1000

    extract select driver into component

---
+++ app/components/Select/SelectDriver.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { Database } from "~~/types/db";
+
+const { allDrivers: drivers } = await useDriver();
+const selectedDriver = defineModel<Database.Driver>();
+</script>
+
+<template lang="pug">
+USelectMenu(:options="drivers?.sort((driverA, driverB) => driverA.familyName.localeCompare(driverB.familyName))" searchable option-attribute="fullName" v-model="selectedDriver")
+  template(#label)
+    .text-faint(v-if="!selectedDriver") Pick a driver
+    div(v-else)
+      DriverOption(:option="selectedDriver")
+  template(#option="{ option }")
+    DriverOption(:option)
+</template>
