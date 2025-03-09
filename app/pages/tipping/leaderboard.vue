a40da77acb9e9c33de515e4f8e31f1a2068670ef
Joschua <70809675+selfire1@users.noreply.github.com>
2025-03-10 07:27:26 +1000

    add placeholder

--- app/pages/tipping/leaderboard.vue
+++ app/pages/tipping/leaderboard.vue
@@ -8,6 +8,11 @@ definePageMeta({
 NuxtLayout(name="tipping")
   template(#page-title)
     | Leaderboard
-  .is-page-height.is-container
-    pre here
+  .is-bg-pattern
+    .is-page-height.is-container
+      .flex.items-center.justify-center
+        UCard
+          .mx-auto.size-8.flex.items-center.justify-center.rounded-full(class="bg-faint/25")
+            UIcon.text-muted.size-5(name="carbon:construction")
+          span.is-display-6 Coming soon
 </template>
