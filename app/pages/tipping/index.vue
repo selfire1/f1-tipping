beaa684f0df76822fbf1215a55aa4b3c275b4552
Joschua <70809675+selfire1@users.noreply.github.com>
2025-03-11 06:55:34 +1000

    move championships card to first

--- app/pages/tipping/index.vue
+++ app/pages/tipping/index.vue
@@ -54,10 +54,30 @@ NuxtLayout(name="tipping")
             TextHero(:level="2" heading="Join or start a group" description="You are not yet a member of a group. Get started tipping by joining or creating a group.")
           UButton(to="/tipping/groups" label="Manage groups")
       template(v-else)
+        template(v-if="championshipCutoffDate && isFuture(championshipCutoffDate)")
+          UCard
+            template(#header)
+              h2.is-display-7 Tip the Championships
+            template(#footer)
+              UButton(label="Tip now" to="/tipping/championships" trailing icon="carbon:arrow-right")
+            .space-y-4
+              .relative.overflow-hidden.py-4.-mx-4.-mt-5(class="sm:-mx-6 sm:-mt-6")
+                AppImg.pointer-events-none.opacity-5.h-full.w-full.object-cover.inset-0.absolute(src="https://images.unsplash.com/photo-1514820720301-4c4790309f46?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+                .px-6
+                  p.is-size-7.text-muted Drivers’ and Constructors’
+                  p.is-display-5 Championships
+              .is-size-7
+                p.is-display-7.flex.items-center.gap-2
+                  | Tipping closes
+                  BadgeTimeTo(:date="championshipCutoffDate")
+                .flex
+                  .pl-2
+                    p {{ championshipCutoffDate.toLocaleString(undefined, { hour: "numeric", minute: "2-digit"}) }}
+                    p {{ championshipCutoffDate.toLocaleString(undefined, { year: "numeric", month: "short", day: "numeric" }) }}
         template(v-if="nextRace")
           UCard
             template(#header)
-              h2.is-display-7 Next Race
+              h2.is-display-7 Predict the next race
             template(#footer)
               UButton(label="Tip now" to="/tipping/add-tips" trailing icon="carbon:arrow-right")
             .space-y-4
@@ -79,24 +99,4 @@ NuxtLayout(name="tipping")
                     p {{ nextRaceCutOffDate.toLocaleString(undefined, { hour: "numeric", minute: "2-digit"}) }}
                     p {{ nextRaceCutOffDate.toLocaleString(undefined, { year: "numeric", month: "short", day: "numeric" }) }}
 
-        template(v-if="championshipCutoffDate && isFuture(championshipCutoffDate)")
-          UCard
-            template(#header)
-              h2.is-display-7 Championships
-            template(#footer)
-              UButton(label="Tip now" to="/tipping/championships" trailing icon="carbon:arrow-right")
-            .space-y-4
-              .relative.overflow-hidden.py-4.-mx-4.-mt-5(class="sm:-mx-6 sm:-mt-6")
-                AppImg.pointer-events-none.opacity-5.h-full.w-full.object-cover.inset-0.absolute(src="https://images.unsplash.com/photo-1514820720301-4c4790309f46?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-                .px-6
-                  p.is-size-7.text-muted Drivers’ and Constructors’
-                  p.is-display-5 Championships
-              .is-size-7
-                p.is-display-7.flex.items-center.gap-2
-                  | Tipping closes
-                  BadgeTimeTo(:date="championshipCutoffDate")
-                .flex
-                  .pl-2
-                    p {{ championshipCutoffDate.toLocaleString(undefined, { hour: "numeric", minute: "2-digit"}) }}
-                    p {{ championshipCutoffDate.toLocaleString(undefined, { year: "numeric", month: "short", day: "numeric" }) }}
 </template>
