ab9aef620999b1ba7b6df08a4882b6c4d925d24a
Joschua <70809675+selfire1@users.noreply.github.com>
2025-03-10 20:02:55 +1000

    update compatibilityDate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
+  compatibilityDate: "2025-03-10",
   css: ["~/assets/css/main.css"],
   devtools: { enabled: false },
   runtimeConfig: {
