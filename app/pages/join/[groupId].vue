f45fc299648730800e307baa323cc7823381af1a
Joschua <70809675+selfire1@users.noreply.github.com>
2025-03-10 21:04:04 +1000

    remove clientonly tag

--- app/pages/join/[groupId].vue
+++ app/pages/join/[groupId].vue
@@ -52,25 +52,22 @@ useSeoMeta({
 <template lang="pug">
 .is-bg-pattern.is-page-height
   .is-container.flex
-    ClientOnly(fallbackTag="div")
-      template(#fallback)
-        USkeleton(class="h-[42px] w-[300px]")
-      UCard.mx-auto.max-w-prose.text-center.w-full
-        template(#header)
-          .space-y-8
-            TextHero(:level="1")
-              template(#heading)
-                h1.flex.flex-col
-                  span.is-size-7.text-muted You’ve been invited to join
-                  template(v-if="status === 'pending'")
-                    USkeleton(class="h-[42px] w-[300px]")
-                  template(v-else)
-                    span.is-display-4 {{ data?.item?.name ?? "Unknown group" }}
-        template(v-if="error")
-          SystemError(:fetch-error="error")
-        template(v-if="!loggedIn")
-          UButton(:to="authPath" label="Log in or sign up to join")
-        template(v-else)
-          UButton(@click="joinGroup" label="Join Group" :loading="isPending" :disabled="isPending")
+    UCard.mx-auto.max-w-prose.text-center.w-full
+      template(#header)
+        .space-y-8
+          TextHero(:level="1")
+            template(#heading)
+              h1.flex.flex-col
+                span.is-size-7.text-muted You’ve been invited to join
+                template(v-if="status === 'pending'")
+                  USkeleton(class="h-[42px] w-[300px]")
+                template(v-else)
+                  span.is-display-4 {{ data?.item?.name ?? "Unknown group" }}
+      template(v-if="error")
+        SystemError(:fetch-error="error")
+      template(v-if="!loggedIn")
+        UButton(:to="authPath" label="Log in or sign up to join")
+      template(v-else)
+        UButton(@click="joinGroup" label="Join Group" :loading="isPending" :disabled="isPending")
 
 </template>
