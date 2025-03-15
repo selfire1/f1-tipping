7948cd11793f6bda7e72e6a39472253417165317
Joschua <70809675+selfire1@users.noreply.github.com>
2025-03-15 16:23:42 +1000

    generate migration

---
+++ migrations/0003_worthless_norman_osborn.sql
@@ -0,0 +1,15 @@
+DROP INDEX "session_token_unique";--> statement-breakpoint
+DROP INDEX "user_email_unique";--> statement-breakpoint
+ALTER TABLE `constructors` ALTER COLUMN "created" TO "created" integer NOT NULL DEFAULT (unixepoch());--> statement-breakpoint
+CREATE UNIQUE INDEX `session_token_unique` ON `session` (`token`);--> statement-breakpoint
+CREATE UNIQUE INDEX `user_email_unique` ON `user` (`email`);--> statement-breakpoint
+ALTER TABLE `constructors` ALTER COLUMN "last_updated" TO "last_updated" integer NOT NULL DEFAULT (unixepoch());--> statement-breakpoint
+ALTER TABLE `drivers` ALTER COLUMN "created" TO "created" integer NOT NULL DEFAULT (unixepoch());--> statement-breakpoint
+ALTER TABLE `group_members` ALTER COLUMN "joined_at" TO "joined_at" integer NOT NULL DEFAULT (unixepoch());--> statement-breakpoint
+ALTER TABLE `groups` ALTER COLUMN "created_at" TO "created_at" integer NOT NULL DEFAULT (unixepoch());--> statement-breakpoint
+ALTER TABLE `prediction_entries` ALTER COLUMN "created_at" TO "created_at" integer NOT NULL DEFAULT (unixepoch());--> statement-breakpoint
+ALTER TABLE `predictions` ALTER COLUMN "created_at" TO "created_at" integer NOT NULL DEFAULT (unixepoch());--> statement-breakpoint
+ALTER TABLE `races` ALTER COLUMN "created" TO "created" integer NOT NULL DEFAULT (unixepoch());--> statement-breakpoint
+ALTER TABLE `results` ALTER COLUMN "added_at" TO "added_at" integer NOT NULL DEFAULT (unixepoch());--> statement-breakpoint
+ALTER TABLE `results` ALTER COLUMN "updated_at" TO "updated_at" integer NOT NULL DEFAULT (unixepoch());--> statement-breakpoint
+ALTER TABLE `results` ADD `constructor_id` text NOT NULL REFERENCES constructors(id);
